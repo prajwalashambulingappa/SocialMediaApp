146ec0dfc3c9a0b10c4da30b811d2b01c071d9a7
Jay <jayasampath111@gmail.com>
2021-12-08 13:34:46 +0530

    Add files via upload

--- src/Component/Post/Post.js
+++ src/Component/Post/Post.js
@@ -1,91 +1,119 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import "../Post/Post.css";
-import Avatar from '@mui/material/Avatar';
+import Avatar from "@mui/material/Avatar";
 import like from "../../images/postlike.jpg";
 import comment from "../../images/postcomment.jpg";
 import share from "../../images/postshare.jpg";
 import { Link } from "react-router-dom";
+import AllComments from "../Comment/AllComments";
 
+const Post = (props) => {
+  const imageResourceUrl = "https://drive.google.com/uc?export=view&id=";
+  const [commentsButtonClicked, setCommentsButtonClicked] = useState(false);
+  const [likesButtonClicked, setLikesButtonClicked] = useState(false);
 
-const Post =(props) => {
+  const closeCommentsModal = () => {
+    setCommentsButtonClicked(false);
+  };
+  const closeLikesModal = () => {
+    setLikesButtonClicked(false);
+  };
+  const openCommentsModal = () => {
+    setCommentsButtonClicked(true);
+  };
+  const openLikesModal = () => {
+    setLikesButtonClicked(true);
+  };
 
-//     // commentList:[]
-//     // componentDidMount(){
-//     //     this.getComments();
-//     // }
-    
-//     const getComments=()=>{
-//         let data=[
-//             {
-//                 "user":"abc",
-//                 "commentid":"2873",
-//                 "timestamp":"2642",
-//                 "description":"Comment"
-//             }
-//         ];
-//         this.setState({commentList: data});
-//     }
-console.log(props);
-const imageResourceUrl = "https://drive.google.com/uc?export=view&id=";
-        return ( 
-            <div className= "post_container">
+  let hashtags = [];
+  for (const tag in props.hashtags) {
+    hashtags.push(props.hashtags[tag]);
+  }
 
-                {/* Header */}
-                <div className= "post_header">
-                    <Avatar className="post_image" src={props.profileimage} alt="post" />
-                    <div className="post_user">{props.user}</div>
-                </div>
+  return (
+    <div className="post_container">
+      {/* Header */}
+      <div className="post_header">
+        <Avatar className="post_image" src={props.profileimage} alt="post" />
+        <div className="post_user">{props.user}</div>
+      </div>
 
-                {/* Image*/}
-                <div>
-                    <img src={imageResourceUrl+ props.postimage} alt="post" width="860rem" height="700rem" />
-                    
-                </div>
+      {/* Image*/}
+      <div>
+        <img
+          src={imageResourceUrl + props.postimage}
+          alt="post"
+          width="860rem"
+          height="700rem"
+        />
+      </div>
 
-                <div className="Caption">
-                    {props.postcaption}
-                </div>
-                <div className="Caption">
-                    {props.time}
-                </div>
+      <div className="Caption">{props.postcaption}</div>
+      <div className="Caption">{props.time}</div>
+      <div>
+        {hashtags.map((tag) => {
+          return (
+            <span className="hashtag-span" key={tag}>
+              {
+              (
+                <Link
+                  to={{
+                    pathname: "/hashtag-posts",
+                    state: {
+                      tagName: tag,
+                    },
+                  }}
+                >
+                  {'#'+tag}
+                </Link>
+              )}
+            </span>
+          );
+        })}
+      </div>
 
-                {/* Like count */}
-                <div>
-                    <div style ={{"marginLeft":"1rem"}}>
-                        <img src= {like} className="post_reactimage" alt="react" />
-                        <img src= {comment} className="post_reactimage" alt="react" />
-                        {/* <img src= {share} className="post_reactimage" alt="react" /> */}
-                    </div>
-                    <div className="dummy_likes">
-                        {props.likes} Likes
-                    </div>
-                    <div className="dummy_likes">
-                        {props.comments} Comments
-                    </div>
+      {/* Like count */}
+      <div>
+        <div style={{ marginLeft: "1rem" }}>
+          <img src={like} className="post_reactimage" alt="react" />
+          <img src={comment} className="post_reactimage" alt="react" />
+          {/* <img src= {share} className="post_reactimage" alt="react" /> */}
+        </div>
+        {props.likes === 0 ? (
+          <p> 0 likes</p>
+        ) : (
+          <div className="dummy_likes">
+            {props.likes} <button onClick={openLikesModal}>Likes </button>{" "}
+          </div>
+        )}
 
-                    <Link  to={{
-                        pathname:"/all-comments",
-                        state:{
-                            postId: props.id
-                        }
-                    }}> see all comments </Link>
-                </div>
+        {props.comments === 0 ? (
+          <p>
+            {" "}
+            <i>no comments yet</i>{" "}
+          </p>
+        ) : (
+          <div className="dummy_likes">
+            {props.comments}{" "}
+            <button onClick={openCommentsModal}> Comments </button>{" "}
+          </div>
+        )}
 
-                {/* Comment section */}
-                {/* <div>
-                    {
-                        this.state.commentList.map((item, index)=>(
-                            <div key={item.id} className="dummy_comments"> 
-                                <div> Comments: </div>
-                                {item.user}: {item.description} 
-                            </div>
-                        ))
-                    } */}
-                    
-                    <input text="text" className="post_commentbox" placeholder=" Add a comment.." />
-                {/* </div> */}
-            </div>
-        );
-   }
+        {commentsButtonClicked && (
+          <AllComments
+            postId={props.id}
+            closeCommentsModal={closeCommentsModal}
+          />
+        )}
+      </div>
+
+      <input
+        text="text"
+        className="post_commentbox"
+        placeholder=" Add a comment.."
+      />
+    </div>
+  );
+};
 
 export default Post;
